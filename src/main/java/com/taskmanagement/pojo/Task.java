6237a709c152b801a753ee77933276c85c429a2c
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-21 23:31:44 +0530

    Add files via upload

--- src/main/java/com/taskmanagement/pojo/Task.java
+++ src/main/java/com/taskmanagement/pojo/Task.java
@@ -1,1 +1,147 @@
+package com.highradius.taskmanagement.pojo;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task")
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+	private int taskId;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "due_date")
+	private Date dueDate;
+	
+	@Column(name = "prority")
+	private String priority;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@Column(name = "assignee")
+	private String assignee;
+	
+	@Column(name = "creator")
+	private String creator;
+	
+	@Column(name = "creation_date")
+	private Date creationDate;
+	
+	@Column(name = "last_modified_date")
+	private Date lastModifiedDate;
+	
+	@Column(name = "completion_date")
+	private Date completionDate;
+
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getAssignee() {
+		return assignee;
+	}
+
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public Date getCompletionDate() {
+		return completionDate;
+	}
+
+	public void setCompletionDate(Date completionDate) {
+		this.completionDate = completionDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [taskId=" + taskId + ", title=" + title + ", description=" + description + ", dueDate=" + dueDate
+				+ ", priority=" + priority + ", status=" + status + ", assignee=" + assignee + ", creator=" + creator
+				+ ", creationDate=" + creationDate + ", lastModifiedDate=" + lastModifiedDate + ", completionDate="
+				+ completionDate + "]";
+	}
+	
+}
