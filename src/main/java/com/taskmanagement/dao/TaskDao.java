52a38df96f626ab4351dbb4c9e74d42a163ee05c
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-21 23:32:29 +0530

    Add files via upload

--- src/main/java/com/taskmanagement/dao/TaskDao.java
+++ src/main/java/com/taskmanagement/dao/TaskDao.java
@@ -1,1 +1,18 @@
+package com.highradius.taskmanagement.dao;
 
+import java.util.List;
+
+import com.highradius.taskmanagement.exceptions.TaskManagementException;
+import com.highradius.taskmanagement.pojo.Task;
+
+public interface TaskDao {
+    
+    List<Task> allTasks() throws TaskManagementException;
+    
+    int insertTask(Task task) throws TaskManagementException;
+    
+    int updateTask(Task task) throws TaskManagementException;
+    
+    int deleteTask(int taskId) throws TaskManagementException;
+
+}
