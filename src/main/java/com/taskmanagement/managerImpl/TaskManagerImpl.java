ffceebe4861b141c8d427d3d69cff4a03caeffa7
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-21 23:35:02 +0530

    Add files via upload

--- src/main/java/com/taskmanagement/managerImpl/TaskManagerImpl.java
+++ src/main/java/com/taskmanagement/managerImpl/TaskManagerImpl.java
@@ -1,1 +1,58 @@
+package com.highradius.taskmanagement.managerImpl;
 
+import java.util.List;
+
+import com.highradius.taskmanagement.dao.TaskDao;
+import com.highradius.taskmanagement.daoImpl.TaskDaoImpl;
+import com.highradius.taskmanagement.exceptions.TaskManagementException;
+import com.highradius.taskmanagement.manager.TaskManager;
+import com.highradius.taskmanagement.pojo.Task;
+
+public class TaskManagerImpl implements TaskManager {
+
+    TaskDao taskDao = new TaskDaoImpl();
+
+    public TaskDao getTaskDao() {
+		return taskDao;
+	}
+
+	public void setTaskDao(TaskDao taskDao) {
+		this.taskDao = taskDao;
+	}
+
+	@Override
+    public List<Task> allTasks() throws TaskManagementException {
+        try {
+            return taskDao.allTasks();
+        } catch (Exception e) {
+            throw new TaskManagementException("Error fetching tasks: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public int insertTask(Task task) throws TaskManagementException {
+        try {
+            return taskDao.insertTask(task);
+        } catch (Exception e) {
+            throw new TaskManagementException("Error creating task: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public int updateTask(Task task) throws TaskManagementException {
+        try {
+            return taskDao.updateTask(task);
+        } catch (Exception e) {
+            throw new TaskManagementException("Error updating task: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public int deleteTask(int taskId) throws TaskManagementException {
+        try {
+            return taskDao.deleteTask(taskId);
+        } catch (Exception e) {
+            throw new TaskManagementException("Error deleting task: " + e.getMessage());
+        }
+    }
+}
