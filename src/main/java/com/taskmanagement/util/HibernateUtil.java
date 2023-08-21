bcf345669425a8409f0a9173dc15caf19e4299ba
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-21 23:35:42 +0530

    Add files via upload

--- src/main/java/com/taskmanagement/util/HibernateUtil.java
+++ src/main/java/com/taskmanagement/util/HibernateUtil.java
@@ -1,1 +1,31 @@
+package com.highradius.taskmanagement.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration().configure();
+            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
 
