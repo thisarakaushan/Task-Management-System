ca3ebed4daf8f977d06a20a08e16eeed77edecbf
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-21 23:37:08 +0530

    Add files via upload

--- src/main/java/com/taskmanagement/exceptions/TaskManagementException.java
+++ src/main/java/com/taskmanagement/exceptions/TaskManagementException.java
@@ -1,1 +1,13 @@
+package com.highradius.taskmanagement.exceptions;
 
+public class TaskManagementException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 4834187531768750222L;
+
+	public TaskManagementException(String message) {
+        super(message);
+    }
+}
