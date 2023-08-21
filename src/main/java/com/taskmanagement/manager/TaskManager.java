9c812d215c358e74cd4a517d07094a2daaf2991a
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-21 23:34:12 +0530

    Add files via upload

--- src/main/java/com/taskmanagement/manager/TaskManager.java
+++ src/main/java/com/taskmanagement/manager/TaskManager.java
@@ -1,1 +1,17 @@
+package com.highradius.taskmanagement.manager;
 
+import java.util.List;
+
+import com.highradius.taskmanagement.exceptions.TaskManagementException;
+import com.highradius.taskmanagement.pojo.Task;
+
+public interface TaskManager {
+
+    List<Task> allTasks() throws TaskManagementException;
+
+    int insertTask(Task task) throws TaskManagementException;
+
+    int updateTask(Task task) throws TaskManagementException;
+
+    int deleteTask(int taskId) throws TaskManagementException;
+}
